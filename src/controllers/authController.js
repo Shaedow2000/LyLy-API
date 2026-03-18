b6b2e4fa21228d038a8e92c7457346a9ab302fc4
Shaedow2000 <shaedow2000@proton.me>
2026-03-18 17:43:30 +0000

    Create register function.

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,18 @@
 import wrapper from "../middlewares/asyncWrapper.js";
+import AccountModel from "../models/account.js";
 
-const register = wrapper( async ( req, res ) => {} );
+const register = wrapper( async ( req, res ) => {
+    const data = req.body;
+
+    const newAccount = new AccountModel( data );
+    newAccount.save();
+
+    return res.status( 201 ).json( {
+        'status': 201,
+        'message': 'Account created successfully!',
+        'account': data
+    } );
+} );
 
 const login = wrapper( async ( req, res ) => {} );
 
