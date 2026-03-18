0356cefca1977a4ce2c61aa9ea2cf82c97ae4896
Shaedow2000 <shaedow2000@proton.me>
2026-03-18 08:56:51 +0000

    Add .env.example file & Add global PORT variable.

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 import express from "express";
+import 'dotenv/config';
 
 const app = express();
 
-app.listen( 5050, () => {
-    console.log( `=> Listening on PORT:5050` );
+const PORT = process.env.PORT;
+
+app.listen( PORT, () => {
+    console.log( `=> Listening on PORT:${ PORT }` );
 } );
