8bf997ca3dccc5a2b6a6bbb01be45e41f0a13677
Shaedow2000 <shaedow2000@proton.me>
2026-04-18 23:06:11 +0100

    Add email validation to schema.

--- src/models/account.js
+++ src/models/account.js
@@ -1,22 +1,24 @@
 import * as mongoose from "mongoose";
+import validator from "validator";
 
-const AccountSchema = new mongoose.Schema( {
-    username: {
-        type: String,
-        required: [ true, 'A username is required.' ]
-    },
-    email: {
-        type: String,
-        required: [ true, 'An e-mail is required.' ],
-        unique: [ true, 'This e-mail is already in use.' ]
-    },
-    password: {
-        type: String,
-        required: [ true, 'A password is required.' ],
-        minLength: [ 6, 'Password should contain at least 6 characters or more.' ]
-    }
-} );
+const AccountSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "A username is required."],
+  },
+  email: {
+    type: String,
+    required: [true, "An e-mail is required."],
+    unique: [true, "This e-mail is already in use."],
+    validate: [validator.isEmail, "E-mail adress is invalid."],
+  },
+  password: {
+    type: String,
+    required: [true, "A password is required."],
+    minLength: [6, "Password should contain at least 6 characters or more."],
+  },
+});
 
-const AccountModel = mongoose.model( 'Account', AccountSchema );
+const AccountModel = mongoose.model("Account", AccountSchema);
 
 export default AccountModel;
