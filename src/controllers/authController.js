0c0671e01a30c87732626e016d2c531b0edbe57c
Shaedow2000 <shaedow2000@proton.me>
2026-04-19 14:43:13 +0100

    Add delete account route & Fix password comparing error.

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -25,7 +25,7 @@ const login = wrapper(async (req, res) => {
   const user = await AccountModel.findOne({ email }, { __v: false });
 
   if (user) {
-    const isUser = await bcrypt.compare(user.password, password);
+    const isUser = await bcrypt.compare(password, user.password);
     if (isUser) {
       return res.status(200).json({
         status: 200,
@@ -48,6 +48,34 @@ const login = wrapper(async (req, res) => {
   }
 });
 
-const deleteAccount = wrapper(async (req, res) => {});
+const deleteAccount = wrapper(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await AccountModel.findOne({ email }, { __v: false });
+
+  if (user) {
+    const isUser = await bcrypt.compare(password, user.password);
+    if (isUser) {
+      await AccountModel.deleteOne({ email });
+      return res.status(202).json({
+        status: 202,
+        message: "Account deleted successfully",
+        account: null,
+      });
+    } else {
+      return res.status(403).json({
+        status: 403,
+        message: "Authentification error",
+        account: null,
+      });
+    }
+  } else {
+    return res.status(404).json({
+      status: 404,
+      message: "Account not found",
+      account: undefined,
+    });
+  }
+});
 
 export { register, login, deleteAccount };
--- src/routes/public.js
+++ src/routes/public.js
@@ -1,11 +1,17 @@
 import express from "express";
 
-import { register, login } from "../controllers/authController.js";
+import {
+  register,
+  login,
+  deleteAccount,
+} from "../controllers/authController.js";
 
 const publicRouter = express.Router();
 
 publicRouter.route("/login").get(login);
 
 publicRouter.route("/register").post(register);
+
+publicRouter.route("/unregister").delete(deleteAccount);
 
 export default publicRouter;
