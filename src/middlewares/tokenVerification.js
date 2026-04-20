64d6e4385639a6f02527e06d3a6ebbfe0a278c44
Shaedow2000 <amsal8795@gmail.com>
2026-04-20 13:13:45 +0100

    Create token verification function.

--- src/middlewares/tokenVerification.js
+++ src/middlewares/tokenVerification.js
@@ -1,10 +1,28 @@
 import jwt from "jsonwebtoken";
 import "dotenv/config";
+import AccountModel from "../models/account";
 
-const tokenVerification = (req, res, next) => {
+const tokenVerification = async (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
-  // TODO: complete this function.
+
+  try {
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await AccountModel.findOne({ email: decodedToken.email });
+
+    if (user) {
+      req.user = user;
+      next();
+    } else {
+      res.status(401).json({
+        status: 401,
+        messgae: "Account not found",
+        account: null,
+      });
+    }
+  } catch (err) {
+    next(err);
+  }
 };
 
 export default tokenVerification;
