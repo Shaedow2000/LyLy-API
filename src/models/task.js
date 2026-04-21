366bb3f85ff9c322cb0954212d7ec13ea934cc75
Shaedow2000 <shaedow2000@proton.me>
2026-04-21 20:37:36 +0100

    Change task schema structure.

--- src/models/task.js
+++ src/models/task.js
@@ -1,12 +1,20 @@
 import * as mongoose from "mongoose";
 
 const TaskSchema = new mongoose.Schema({
-  id: Number,
-  title: String,
-  text: {
-    type: String,
-    default: "No task yet!",
+  user: {
+    type: { type: String },
+    required: true,
   },
+  task: [
+    {
+      id: { type: Number },
+      title: { type: String },
+      text: {
+        type: String,
+        default: "No tasks yet!",
+      },
+    },
+  ],
 });
 
 const TaskModel = mongoose.model("Task", TaskSchema);
