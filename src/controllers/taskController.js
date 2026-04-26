9aaf0b0808b1f1728c18966e34804a1bd1ce583d
Shaedow2000 <shaedow2000@proton.me>
2026-04-26 12:17:28 +0100

    Fix delete function.

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -84,7 +84,7 @@ const deleteById = wrapper(async (req, res) => {
   const userEmail = req.user.email;
   const data = await TaskModel.findOne({ user: userEmail }, { __v: false });
 
-  if (!data.tasks.find((task) => String(task._id) !== req.params.id)) {
+  if (!data.tasks.find((task) => String(task._id) === req.params.id)) {
     return res.status(404).json({
       status: 404,
       message: `Task with id [ ${req.params.id} ] was not found`,
